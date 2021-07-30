998ddb3d0ad8d020a49afca3467db349d9de41d3
RamanChola <ramanchola3@gmail.com>
2021-07-31 03:47:04 +0530

    mdake it betterd

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
@@ -18,13 +19,7 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-  const cors = require('cors');
-  const corsOptions ={
-      origin:'http://localhost:3000', 
-      credentials:true,            //access-control-allow-credentials:true
-      optionSuccessStatus:200
-  }
-  app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.get("/uploads/images/:key", (req, res) => {
